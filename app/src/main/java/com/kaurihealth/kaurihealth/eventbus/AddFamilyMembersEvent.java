3f84dbe3175e6c0368e763606dc2ddbb3d95cecd
jianghw <jhw89@163.com>
2017-02-10 09:27:58 +0800

    年后修改提交

---
+++ app/src/main/java/com/kaurihealth/kaurihealth/eventbus/AddFamilyMembersEvent.java
@@ -0,0 +1,13 @@
+package com.kaurihealth.kaurihealth.eventbus;
+
+/**
+ * Created by mip on 2017/2/6.
+ * Describe: 添加完家庭成员后刷新事件
+ */
+
+public class AddFamilyMembersEvent {
+
+    public AddFamilyMembersEvent(){
+
+    }
+}
